4c2e01fbce72c08ebfaf1520f3f28ce511bc8be9
Sergei-Okhlopkov <s_okhlopkov@mail.ru>
2021-04-19 19:24:27 +0300

    Program is ready. No Unit tests

--- Matrix/MainWindow.xaml.cs
+++ Matrix/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace Matrix
 {
@@ -23,6 +14,91 @@ namespace Matrix
         public MainWindow()
         {
             InitializeComponent();
+           
+        }
+
+        MatrixClass<int> A;
+        MatrixClass<int> B;
+        MatrixClass<int> S;
+        Stopwatch sw = new Stopwatch();
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            Gen();
+        }
+
+        public void Gen()
+        {
+            A = new MatrixClass<int>(Convert.ToInt32(txtSizeMatrix.Text));
+            txtMatrixA.Text = A.Print(A);
+            B = new MatrixClass<int>(Convert.ToInt32(txtSizeMatrix.Text));
+            txtMatrixB.Text = B.Print(B);
+
+        }
+
+        private void btnSum_Click(object sender, RoutedEventArgs e)
+        {
+            
+            sw.Start();
+
+            Sum();
+
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:0000}",
+           ts.Seconds,
+            ts.Milliseconds);
+            txtTime.Text = elapsedTime;
+            sw.Reset();
+        }
+
+        public void Sum()
+        {
+            S = A + B;
+            txtMatrixS.Text = S.Print(S);
+        }
+
+        private void btnMult_Click(object sender, RoutedEventArgs e)
+        {
+            sw.Start();
+
+            Multiplication();
+
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}",
+           ts.Seconds,
+            ts.Milliseconds / 10);
+            txtTime.Text = elapsedTime;
+            sw.Reset();
+        }
+
+        public void Multiplication()
+        {
+            S = A * B;
+            txtMatrixS.Text = S.Print(S);
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveToFile(txtMatrixS.Text);
+        }
+
+        public void SaveToFile(string text)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Comma-Separated Values|*.csv";
+            saveFileDialog1.Title = "Сохраняем csv файл";
+            saveFileDialog1.ShowDialog();
+
+
+            if (saveFileDialog1.FileName != "")
+            {
+                using (var writer = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    writer.Write(text);
+                }
+            }
         }
     }
 }
---
+++ Matrix/MatrixClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Matrix
+{
+    public class MatrixClass<T>
+    {
+
+        int N;
+        T[,] mas;
+
+        Func<int, int, T[,]> generate = Fill;//делегат
+
+        public MatrixClass(int N)
+        {
+            this.N = N;
+            this.mas = new T[N, N];
+            this.mas = this.Generate(generate, N);//используем делегат тут
+        }
+
+        public T this[int i, int j]//индексатор
+        {
+            get
+            {
+                return mas[i, j];
+            }
+
+            set
+            {
+                mas[i, j] = value;
+            }
+        }
+
+        public T[,] Generate(Func<int, int, T[,]> generate, int N)
+        {
+            this.mas = generate(N, N);
+            return this.mas;
+        }
+        public static T[,] Fill(int i, int j)
+        {
+            T[,] cMas = new T[i, j];
+            var rand = new Random();
+            for (int l = 0; l < i; l++)
+            {
+                for (int m = 0; m < j; m++)
+                {
+                    cMas[l, m] = (dynamic)rand.Next(-11, 11);
+                }
+            }
+            return cMas;
+        }
+
+
+        public static MatrixClass<T> operator +(MatrixClass<T> one, MatrixClass<T> two)
+        {
+            MatrixClass<T> mat = new MatrixClass<T>(one.N);
+            for (int i = 0; i < mat.N; i++)
+            {
+                for (int j = 0; j < mat.N; j++)
+                {
+                    mat[i, j] = (dynamic)one[i, j] + two[i, j];
+                }
+            }
+            return mat;
+        }
+
+        public static MatrixClass<T> operator *(MatrixClass<T> one, MatrixClass<T> two)
+        {
+            MatrixClass<T> mat = new MatrixClass<T>(one.N);
+
+            for (int i = 0; i < mat.N; i++)
+            {
+                for (int j = 0; j < mat.N; j++)
+                {
+                    mat[i, j] = (dynamic)0;
+                    for (int k = 0; k < mat.N; k++)
+                    {
+                        mat[i, j] += (dynamic)one[i, k] * two[k, j];
+                    }
+                }
+            }
+
+            return mat;
+        }
+
+        public string Print(MatrixClass<T> matrix)
+        {
+            string outmas = null;
+
+            for (int i = 0; i < matrix.N; i++)
+            {
+                for (int j = 0; j < matrix.N; j++)
+                {
+                    outmas += Convert.ToString(matrix[i, j]) + " ";
+                }
+                outmas+="\n";
+            }
+
+
+            return outmas;
+        }
+
+    }
+}
